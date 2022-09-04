fcab6cf3a9d56144dfec432ab9a4c83f8804f207
Jonas Lit <melynasdangus@gmail.com>
2022-09-04 12:54:40 +0300

    Updates

---
+++ component---src-pages-index-js-2321f6596703f8b46d7f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkso_breathe=self.webpackChunkso_breathe||[]).push([[678],{6558:function(e,t,n){n.r(t),n.d(t,{Head:function(){return r}});var o=n(7294);Object.assign({},{color:"#8954A8",fontWeight:"bold",fontSize:16,verticalAlign:"5%"},{listStyleType:"none",marginBottom:24});t.default=function(){return o.createElement("main",null,"The best app for breathing is coming soon 😍")};var r=function(){return o.createElement("title",null,"Home Page")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-2321f6596703f8b46d7f.js.map
